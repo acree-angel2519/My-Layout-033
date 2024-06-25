ea4f681b9ec0c1544b31f1a414e7c344bbd83636
acree-angel2519 <arceethicker@gmail.com>
2024-06-25 14:46:29 +0700

    ชลกร พระชั้นนอก 033

---
+++ 033page.js
@@ -0,0 +1,38 @@
+export default function Home() {
+  return (
+    <>
+    <div className="container-fluid">
+      <div className="row text-center">
+        <div className="col-md-12 bg-light p-5">
+         LOVE
+        </div>
+
+
+        <div className="col-md-12 bs-secondary-bg p-5">
+         DEATHS
+        </div>
+
+
+        <div className="row text-center">
+          <div className="col-md-4 bg-warning p-5">
+            01
+          </div>
+          <div className="col-md-4 bg-danger p-5">
+            02
+          </div>
+          <div className="col-md-4 bg-primary p-5">
+            03
+          </div>
+        </div>
+
+
+        <div className="col-md-12 bg-success p-5">
+         ROBOTS
+        </div>
+
+
+      </div>
+    </div>
+    </>
+  );
+}  
